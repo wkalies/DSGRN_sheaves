6dde88368b0b24fefa7eff000360b87a5c323921
Alex Dowling <alex@dowlinghome.com>
2024-06-18 21:40:23 -0400

    Added isola query inheritance

--- src/DSGRN_sheaves/Queries.py
+++ src/DSGRN_sheaves/Queries.py
@@ -65,22 +65,40 @@ class HysteresisQuery(BifurcationQuery):
         super().__init__(parameter_graph, vertices, edges, 
                          param_stability, match_grading, coho_criteria)
 
-def isola_query(parameter_graph, param_stability=None):
+class IsolaQuery(BifurcationQuery):
 
-    if param_stability is None:
-        param_stability = DSGRN_utils.StabilityQuery(parameter_graph.network)
+    def __init__(self, parameter_graph, param_stability=None):
+        if param_stability is None:
+            param_stability = DSGRN_utils.StabilityQuery(parameter_graph.network)
+        
+        vertices = ['a','b','c']
+        edges = [('a','b'), ('b','c')]
+        match_grading = {1 : ['a','c'], 2 : ['b']}
+        coho_criteria = [{
+                          'predicate' : lambda sc : len(sc[0]) == 2,
+                          'dim' : 1,
+                          'clean_stalks' : True
+                         }]    
+        
+        super().__init__(parameter_graph, vertices, edges, 
+                         param_stability, match_grading, coho_criteria)
+
+# def isola_query(parameter_graph, param_stability=None):
+
+#     if param_stability is None:
+#         param_stability = DSGRN_utils.StabilityQuery(parameter_graph.network)
     
-    vertices = ['a','b','c']
-    edges = [('a','b'), ('b','c')]
-    match_grading = {1 : ['a','c'], 2 : ['b']}
-    coho_criteria = [{
-                      'predicate' : lambda sc : len(sc[0]) == 2,
-                      'dim' : 1,
-                      'clean_stalks' : True
-                     }]
+#     vertices = ['a','b','c']
+#     edges = [('a','b'), ('b','c')]
+#     match_grading = {1 : ['a','c'], 2 : ['b']}
+#     coho_criteria = [{
+#                       'predicate' : lambda sc : len(sc[0]) == 2,
+#                       'dim' : 1,
+#                       'clean_stalks' : True
+#                      }]
 
-    return BifurcationQuery(parameter_graph, vertices, edges, param_stability,
-                            match_grading, coho_criteria)
+#     return BifurcationQuery(parameter_graph, vertices, edges, param_stability,
+#                             match_grading, coho_criteria)
 
 def cusp_query(parameter_graph, param_stability=None):
 
