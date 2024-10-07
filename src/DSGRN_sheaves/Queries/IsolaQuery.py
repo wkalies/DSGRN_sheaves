0b08a75f6619d0e25501a58324aaa6cb0791591e
Bill Kalies <william.kalies@utoledo.edu>
2024-10-06 23:39:20 -0400

    Update IsolaQuery.py

--- src/DSGRN_sheaves/Queries/IsolaQuery.py
+++ src/DSGRN_sheaves/Queries/IsolaQuery.py
@@ -3,22 +3,32 @@ from .BifurcationQuery import *
 
 class IsolaQuery(BifurcationQuery):
 
-    def __init__(self, parameter_graph, param_stability=None,SN_check=True,ppath=None):
+    def __init__(self, parameter_graph, param_stability=None,SN_check=True,num_bistable_nodes=1,ppath=None):
         if param_stability is None:
             param_stability = DSGRN_utils.StabilityQuery(parameter_graph.network())
         
-        vertices = ['a','b','c']
-        edges = [('a','b'), ('b','c')]
-        match_grading = {1 : ['a','c'], 2 : ['b']}
+        # vertices = ['a','b','c']
+        # edges = [('a','b'), ('b','c')]
+        # match_grading = {1 : ['a','c'], 2 : ['b']}
 
+        vertices = ['a','b1']
+        edges = [('a','b1')]
+        for i in range(2,num_bistable_nodes+1):
+            vertices.append('b'+str(i))
+            edges.append(('b'+str(i-1),'b'+str(i)))
+        vertices.append('c')
+        edges.append(('b'+str(num_bistable_nodes),'c'))
+        bistable_vertices=vertices[1:num_bistable_nodes+1]
+        match_grading = {1 : ['a','c'], 2 : bistable_vertices}
+        
         if SN_check:
             coho_criteria = [
-                             {'selection' : ['a','b'],
+                             {'selection' : ['a','b1'],
                               'predicate' : lambda sc : len(sc[0]) == 2, # SN                       
                               'dim' : 1,
                               'clean_stalks' : True
                              }, 
-                             {'selection' : ['b','c'],
+                             {'selection' : ['b'+str(num_bistable_nodes),'c'],
                               'predicate' : lambda sc : len(sc[0]) == 2, # SN                       
                               'dim' : 1,
                               'clean_stalks' : True
